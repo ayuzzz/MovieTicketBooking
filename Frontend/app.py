c3af003b09f4ab792f208fe5147e7c893b5e7514
Ayush Joshi <joshiayush2426@gmail.com>
2020-04-06 04:01:52 +0530

    navigation and templates added.
    templates, routes and basic navbar added for the sake of navigation.

--- Frontend/app.py
+++ Frontend/app.py
@@ -4,9 +4,32 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html", message = "Hi there !!")
+def defaultroute():
+    try:
+        return "Connection to MTB app successful !!"
+    except Exception as ex:
+        return ex
+
+
+@app.route('/home')
+def homepage():
+    return render_template("index.html")
+
+
+@app.route('/myProfile')
+def profile():
+    return render_template("profile.html")
+
+
+@app.route('/myWallet')
+def wallet():
+    return render_template("wallet.html")
+
+
+@app.route('/wishlist')
+def wishlist():
+    return render_template("wishlist.html")
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ Frontend/static/js/base.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+  $('.second-button').on('click', function () {
+
+    $('.animated-icon2').toggleClass('open');
+  });
+
+  $('.nav-item').on('click',function(){
+
+  //Remove any previous active classes
+  $('.nav-item').removeClass('active');
+
+  //Add active class to the clicked item
+  $(this).addClass('active');
+  });
+});
+
